1ce8f64545107fe312407d054a63ad976aa16a07
Ctyode <ctyode@gmail.com>
2016-04-01 21:35:41 -0700

    http request to /anirandom.json

---
+++ app/src/main/java/com/flamie/anirandom/HttpRequest.java
@@ -0,0 +1,49 @@
+package com.flamie.anirandom;
+
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class HttpRequest extends Thread {
+
+    public interface HttpRequestCallback {
+        void success(String data);
+        void error(Exception e);
+    }
+
+    private URL url;
+    private HttpRequestCallback callback;
+
+    public HttpRequest(URL url, HttpRequestCallback callback) {
+        this.url = url;
+        this.callback = callback;
+    }
+
+    @Override
+    public void run() {
+        try {
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String data;
+            StringBuffer dataBuffer = new StringBuffer();
+            String line;
+            while ((line = br.readLine()) != null) {
+                dataBuffer.append(line);
+            }
+            data = dataBuffer.toString();
+            br.close();
+            urlConnection.disconnect();
+            callback.success(data);
+        } catch (IOException e) {
+            callback.error(e);
+        }
+    }
+}
--- app/src/main/java/com/flamie/anirandom/RandomActivity.java
+++ app/src/main/java/com/flamie/anirandom/RandomActivity.java
@@ -3,16 +3,29 @@ package com.flamie.anirandom;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 public class RandomActivity extends AppCompatActivity {
 
@@ -36,12 +49,47 @@ public class RandomActivity extends AppCompatActivity {
         randomize.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        "Пора покормить деда!", Toast.LENGTH_LONG);
-                toast.show();
+                try {
+                    HttpRequest httpRequest = new HttpRequest(new URL("http://192.168.0.103:8080/anirandom.json"), new HttpRequest.HttpRequestCallback() {
+                        @Override
+                        public void success(String data) {
+                            System.out.println(data);
+                            System.out.println("ебни, просто ебни");
+                        }
+
+                        @Override
+                        public void error(Exception e) {
+                            Log.e(getClass().getName(), "все наебнулось", e);
+                        }
+                    });
+                    httpRequest.start();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
             }
         });
+
+        Spinner genres = (Spinner) findViewById(R.id.genre);
+        ArrayAdapter<CharSequence> genresAdapter = ArrayAdapter.createFromResource(this,
+                R.array.genre, android.R.layout.simple_spinner_item);
+        genresAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genres.setAdapter(genresAdapter);
+
+        Spinner years = (Spinner) findViewById(R.id.year);
+        ArrayAdapter<CharSequence> yearsAdapter = ArrayAdapter.createFromResource(this,
+                R.array.year, android.R.layout.simple_spinner_item);
+        yearsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        years.setAdapter(yearsAdapter);
+
+        Spinner rating = (Spinner) findViewById(R.id.rating);
+        ArrayAdapter<CharSequence> ratingAdapter = ArrayAdapter.createFromResource(this,
+                R.array.rating, android.R.layout.simple_spinner_item);
+        ratingAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        rating.setAdapter(ratingAdapter);
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
